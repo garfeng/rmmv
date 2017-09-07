6dc1a1a55b1cf1f1555dd0a9b4e67fe761d9ff5e
garfeng <garfeng_gu@163.com>
2017-09-07 17:07:47 +0800

    Create load_jquery.js

---
+++ load_jquery.js
@@ -0,0 +1,5 @@
+function loadJquery(){
+  var script = document.createElement("script");
+  script.src = "https://cdn.bootcss.com/jquery/3.2.1/jquery.js";
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
